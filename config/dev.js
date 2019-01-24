137b79e38318fbe08f46ce36ec6f99249e441331
avral <qweasd_96@mail.ru>
2019-01-24 17:28:44 +0800

    impl multiple chain config

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,13 @@
-export default {
+const local = {
+  contract: 'ordersbook',
+
+  chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
+  host: 'http://localhost:8888',
+  port: 8888,
+  protocol: 'http',
+}
+
+const jungle = {
   contract: 'avralsjungle',
 
   chainId: 'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473',
@@ -6,3 +15,10 @@ export default {
   port: 80,
   protocol: 'https',
 }
+
+const eos_mainet = {
+  // TODO Implement mainet
+
+}
+
+export default process.env.isDev ? local : jungle
