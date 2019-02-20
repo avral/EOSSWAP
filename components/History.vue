bae0173bb20da36f50207452a0fc66fe78fd37c0
avral <qweasd_96@mail.ru>
2019-02-20 10:23:14 +0800

    fix table size history

--- components/History.vue
+++ components/History.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
 //el-table(:data="history", :prop="{prop: 'block_num', order: 'ascending'}" style='width: 100%')
 el-table(:data="history", row-key="block_num'" style='width: 100%')
-    el-table-column(prop='maker', label='Seller', width='100')
-    el-table-column(prop='buyer', label='Buyer', width='100')
-    el-table-column(prop='sell', label='Sell', width='200')
-    el-table-column(prop='buy', label='Buy', width='200')
+    el-table-column(prop='maker', label='Seller', width='110')
+    el-table-column(prop='buyer', label='Buyer', width='110')
+    el-table-column(prop='sell', label='Sell', width='210')
+    el-table-column(prop='buy', label='Buy', width='210')
 
-    el-table-column(prop='time', label='Time', width='230')
+    el-table-column(prop='time', label='Time', width='220')
 </template>
 
 <script>
@@ -40,7 +40,8 @@ export default {
         limit: 100
       }).then(r => {
         this.history = r.rows.map(h => {
-          h.time = new Date(h.time * 1000).toLocaleString();
+          let t = new Date(h.time * 1000).toLocaleString().split(':')
+          h.time = t[0] + ':' + t[1] + ' ' + t[2].split(' ')[1];
 
           h.sell = h.sell.quantity + '@' + h.sell.contract;
           h.buy = h.buy.quantity + '@' + h.buy.contract;
