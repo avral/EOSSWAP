bb866ba621bafb4b65aece36e8db16dfd6581840
avral <qweasd_96@mail.ru>
2019-02-21 15:31:05 +0800

    update config

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const jungle = {
 
 const eos_mainet = {
   name: 'eos',
-  contract: 'avralsjungle',
+  contract: 'wwweosswapio',
 
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
 
