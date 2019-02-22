2f2220299041027c3b8a81ea519c74829933ce9b
avral <qweasd_96@mail.ru>
2019-02-22 18:06:59 +0800

    generate token logo link

--- plugins/filters.js
+++ plugins/filters.js
@@ -5,3 +5,8 @@ import config from '~/config'
 Vue.filter('monitorAccount', function (account) {
   return `${config.monitor}/account/${account}`
 })
+
+
+Vue.prototype.$tokenLogo = function(symbol, contract) {
+  return `https://raw.githubusercontent.com/BlockABC/eos-tokens/master/tokens/${contract}/${symbol}.png`
+}
