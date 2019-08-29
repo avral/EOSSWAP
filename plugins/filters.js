ab96aa5aa23d4f4361c2d38f6c3096527bc2407c
avral <qweasd_96@mail.ru>
2019-08-29 19:07:54 +0300

    add TKT icon

--- plugins/filters.js
+++ plugins/filters.js
@@ -11,5 +11,8 @@ Vue.prototype.$tokenLogo = function(symbol, contract) {
   if (symbol == 'PIXEOS' && contract == 'pixeos1token')
     return 'https://pixeos.io/ico/apple-touch-icon-57-precomposed.png'
 
+  if (symbol == 'TKT' && contract == 'eossanguotkt')
+    return require('@/assets/tokens/tkt_eossanguotkt.png')
+
   return `https://raw.githubusercontent.com/BlockABC/eos-tokens/master/tokens/${contract}/${symbol}.png`
 }
